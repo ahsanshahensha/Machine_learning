82291cb6c8bdf8e89e51eeed1b9cfc84075a389a
Ahsan Shahenshah <ahsan.shahenshah@s2014.tu-chemnitz.de>
2017-07-17 19:31:03 +0200

    no message

---
+++ t_flow.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 15 11:32:31 2017
+
+@author: MSI
+"""
+import os
+import tensorflow as tf
+import skimage
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+# Initialize Session and run `result`
+with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+  config=tf.ConfigProto(log_device_placement=True)
+  output = sess.run(result)
+  print(output)
+  
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory) 
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_dir = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_dir, f) 
+                      for f in os.listdir(label_dir) 
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+ROOT_PATH = ""
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+images, labels = load_data(train_data_directory)
+images= np.array(images)
+#labels=np.array(labels)
+
+plt.hist(labels,62)
+plt.show()
+
+# Determine the (random) indexes of the images that you want to see 
+traffic_signs = [300, 2250, 3650, 4000]
+
+# Fill out the subplots with the random images that you defined 
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images[traffic_signs[i]])
+    plt.subplots_adjust(wspace=0.5)
+
+plt.show()
+
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(images[traffic_signs[i]])
+    plt.subplots_adjust(wspace=0.5)
+    plt.show()
+    print("shape: {0}, min: {1}, max: {2}".format(images[traffic_signs[i]].shape, 
+                                                  images[traffic_signs[i]].min(), 
+                                                  images[traffic_signs[i]].max()))
+    
+
+# Get the unique labels 
+unique_labels = set(labels)
+
+# Initialize the figure
+plt.figure(figsize=(15, 15))
+
+# Set a counter
+i = 1
+
+# For each unique label,
+for label in unique_labels:
+    # You pick the first image for each label
+    image = images[labels.index(label)]
+    # Define 64 subplots 
+    plt.subplot(8, 8, i)
+    # Don't include axes
+    plt.axis('off')
+    # Add a title to each subplot 
+    plt.title("Label {0} ({1})".format(label, labels.count(label)))
+    # Add 1 to the counter
+    i += 1
+    # And you plot this first image 
+    plt.imshow(image)
+    
+# Show the plot
+plt.show()
+    
+
+# Rescale the images in the `images` array
+images28 = [skimage.transform.resize(image, (28, 28)) for image in images]
+            
+#to Gray scale
+img_gray=np.array([skimage.color.rgb2gray(im) for im in images28])
+
+traffic_signs = [300, 2250, 3650, 4000]
+
+for i in range(len(traffic_signs)):
+    plt.subplot(1, 4, i+1)
+    plt.axis('off')
+    plt.imshow(img_gray[traffic_signs[i]], cmap="gray")
+    plt.subplots_adjust(wspace=0.5)
+    
+# Show the plot
+plt.show()
